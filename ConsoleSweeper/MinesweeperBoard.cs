97f47797659f77a46e328b18afd68dadea2852ec
frenzardead <frenzardead@gmail.com>
2023-01-21 16:58:37 -0600

    Add project files.

---
+++ ConsoleSweeper/MinesweeperBoard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleSweeper
+{
+    public class MinesweeperBoard
+    {
+        bool[,] brd;
+
+        public MinesweeperBoard(int width, int height, int mines)
+        {
+            if (mines >= width * height) throw new ArgumentOutOfRangeException();
+            brd = new bool[width, height];
+
+            Random rnd = new Random();
+
+            for(int i = 0; i < mines; i++)
+            {
+                while (true) 
+                {
+                    int x = rnd.Next(0, width);
+                    int y = rnd.Next(0, height);
+                    if (!brd[x,y])
+                    {
+                        brd[x, y] = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleSweeper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleSweeper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MinesweeperBoard board = new MinesweeperBoard(15,15, 45);
+        }
+    }
+}
